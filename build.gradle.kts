03df883d72b75442d5a4d475e74fe02225a835b7
kimminseok <alstjr706@naver.com>
2025-11-01 19:52:26 +0900

    chore: 로깅을 위한 SLF4J 및 Logback 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.27.6")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("ch.qos.logback:logback-classic:1.5.20")
 }
 
 tasks.test {
