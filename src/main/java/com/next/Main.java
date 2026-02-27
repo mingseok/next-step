49fb1fe001e20aae4147f85bf8d1190aff50bc93
mingseok <alstjr706@naver.com>
2026-02-27 09:37:18 +0900

    chore: 코드 경고 수정 및 파일 제거

--- src/main/java/com/next/Main.java
+++
@@ -1,6 +0,0 @@
-package com.next;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
--- src/main/java/com/next/webserver/WebServer.java
+++ src/main/java/com/next/webserver/WebServer.java
@@ -11,17 +11,16 @@ public class WebServer {
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
+    public static void main(String[] args) throws Exception {
+        int port;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
         } else {
             port = Integer.parseInt(args[0]);
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
-            log.info("Web Application Server started {} port.", port);
+            log.info("웹 애플리케이션 서버가 시작되었습니다. {} port.", port);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
